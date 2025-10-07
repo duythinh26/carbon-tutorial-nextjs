33ce8ed2d730f50f8d3b55a55c5b19bf3bd999cc
Duy Thinh <thinh.phamduy@xiontechs.com>
2025-10-07 13:58:01 +0700

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import LandingPage from './home/page';
+import RepoPage from './repos/page';
 
-export default function Home() {
+export default function Page() {
   return (
-    <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
-    </div>
-
-  )
+    <>
+      <LandingPage />
+      <RepoPage />
+    </>
+  );
 }
